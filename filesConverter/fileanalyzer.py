fd791db334685439719d4b4e07b30838e2266677
dimcrap <mamad.rahmani235@gmail.com>
2025-12-29 19:23:41 +0330

    implementing helper function and analyazer for usage

--- filesConverter/fileanalyzer.py
+++ filesConverter/fileanalyzer.py
@@ -32,3 +32,22 @@ def is_num(input):
         return True
     except ValueError:
         return False
+    
+def extractName(path):
+    nameS=path.find("/f")
+    nameE=path.find(".txt")
+    name=""
+    for i in range(nameS+1,nameE):
+        name=name+path[i]
+    return name
+
+def makeDict(path):
+    return {"filename":extractName(path),"wordsCount":analyzer.countWord(path),
+            "numberscount":analyzer.countNumbs(path),"uniquewords":analyzer.uniqueWcount(path)}
+
+def make_dictlist(pathlist):
+    list=[{}]
+    for path in pathlist:
+        m_dict=analyzer.countNumbs(path)
+        list.append(m_dict)
+    return list
--- filesConverter/main.py
+++ filesConverter/main.py
@@ -1,11 +1,14 @@
 import csv
 import json
-from fileanalyzer import analyzer
+from fileanalyzer import analyzer,make_dictlist
+
 
 csvfile=None
 #filename,total_words,unique_words,avg_word_length
 
-fpath="files/file2.txt"
+paths=["files/file1.txt","files/file2.txt","files/file3.txt","files/file4.txt"]
+
+
 
 '''
 with open(fpath,mode='r') as file:
@@ -14,6 +17,18 @@ with open(fpath,mode='r') as file:
     csvfile=csv.reader(file)
     for line in csvfile:
         print(line[1])'''
+
+
+'''
 print("word count:",analyzer.countWord(fpath))
 print("numb count:",analyzer.countNumbs(fpath))
-print("unique words count:",analyzer.uniqueWcount(fpath))
+print("unique words count:",analyzer.uniqueWcount(fpath))'''
+
+alldicts=make_dictlist(paths)
+print("filese in convertion process:",paths)
+input("press Enter to convert ...")
+
+
+with open('allfileslog.csv','w',newline='') as csvfile:
+    fields=alldicts[0].keys()
+    writer=csv.DictWriter(csvfile,fieldnames=fields)
