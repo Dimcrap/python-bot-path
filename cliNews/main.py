44022c17b43c7c776cb3db536c0149b0ea01f74a
dimcrap <mamad.rahmani235@gmail.com>
2026-01-01 20:06:08 +0330

    finshing and debuggin cliwheather project-starting clinews project-implementing main functions structures and primary tests

---
+++ cliNews/main.py
@@ -0,0 +1,86 @@
+import requests
+import json
+import secrets
+
+articles=None
+
+def extractUsfulData(artcId):
+    artcNum=None
+    for index,article in articles:
+        if article['article_id']==artcId:
+            artcNum=index
+            break
+    if artcNum==None:
+        print("didn't find the article")
+
+    articleDict={
+        "title":articles[artcNum]['title'],
+        "link":articles[artcNum]['link'],
+        #descreaption
+        "creator":articles[artcNum]['creator'],
+        "country":articles[artcNum]['country'],
+        "image":articles[artcNum]['image_url'],
+    }
+    return articleDict    
+    
+def searchbyCategory(category):
+    list=None
+    for article in articles:
+        if category in article['category'].values():
+            list.append(article["article_id"])
+    return list
+    
+
+def getAarticle(category):
+    #print(category.lower())
+    Rarticle=None
+    catsList=searchbyCategory(category.lower())
+    print(catsList)
+    '''
+    if not catsList:
+        print(f"failed to load aticles with '{category}' as category ")
+    else:
+        Rarticle=secrets.choice(catsList)'''
+    
+
+categories=['Domestic','Tourism','World','Environment','Top']
+m_url="https://newsdata.io/api/1/latest?apikey=pub_fd65230a260d4cdeafc9fe6568a593fc&q=news&category=top,tourism,world,environment,domestic"
+responde=requests.get(m_url)
+data=None
+
+if responde.status_code!=200:
+    print("failed to get newsdata api")
+else:
+    #print("got the respons")
+    data=responde.json()
+
+articles=data['results']
+#print("article count",len(articles))
+#print(articles[4])
+
+print(r"""\                       __.....__                   .     .--.   _..._                          _..._         __.....__                      
+||                  .-''         '.               .'|     |__| .'     '.   .--./)             .'     '.   .-''         '.         _     _    
+||        .-,.--.  /     .-''"'-.  `.           .'  |     .--..   .-.   . /.''\\             .   .-.   . /     .-''"'-.  `. /\    \\   //    
+||  __    |  .-. |/     /________\   \    __   <    |     |  ||  '   '  || |  | |            |  '   '  |/     /________\   \`\\  //\\ //     
+||/'__ '. | |  | ||                  | .:--.'.  |   | ____|  ||  |   |  | \`-' /             |  |   |  ||                  |  \`//  \'/ _    
+|:/`  '. '| |  | |\    .-------------'/ |   \ | |   | \ .'|  ||  |   |  | /("'`              |  |   |  |\    .-------------'   \|   |/.' |   
+||     | || |  '-  \    '-.____...---.`" __ | | |   |/  . |  ||  |   |  | \ '---.            |  |   |  | \    '-.____...---.    '    .   | / 
+||\    / '| |       `.             .'  .'.''| | |    /\  \|__||  |   |  |  /'""'.\           |  |   |  |  `.             .'        .'.'| |// 
+|/\'..' / | |         `''-...... -'   / /   | |_|   |  \  \   |  |   |  | ||     ||          |  |   |  |    `''-...... -'        .'.'.-'  /  
+'  `'-'`  |_|                         \ \._,\ '/'    \  \  \  |  |   |  | \'. __//           |  |   |  |                         .'   \_.'   
+                                       `--'  `"'------'  '---''--'   '--'  `'---'            '--'   '--'                                     """)
+input("press enter to start ...")
+
+print(f"choose between these categories:\n1-{categories[0]}\n"
+      f"2-{categories[1]}\n3-{categories[2]}\n4-{categories[3]}\n5-{categories[4]}")
+select=input()
+while select.isnumeric()==False or int(select)<1 or int(select)>5:
+    print(f"unvalid input!\nchoose between these categories:\n1-{categories[0]}\n"
+          f"2-{categories[1]}\n3-{categories[2]}\n4-{categories[3]}\n5-{categories[4]}")
+    select=input()
+
+outputarticle=getAarticle((categories[int(select)-1]))
+#outputarticle=getAarticle('top')
+#print(extractUsfulData(outputarticle['article_id']))
+#print(outputarticle)
+#print(articles)
--- cliwheater/main.py
+++ cliwheater/main.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import numpy as np
+import pandas as pd
 
 API_key="the key ..."
 latitude=00000
@@ -9,17 +11,59 @@ exclude_port="minutely,hourly"
 
 url = "https://api.open-meteo.com/v1/forecast"
 params = {
-    "latitude": 40.7128,
-    "longitude": -74.0060,
-    "hourly": "temperature_2m,precipitation,windspeed_10m",
+    "latitude": 32,
+	"longitude": 53,
+	"daily": ["apparent_temperature_max", "snowfall_sum", "sunrise", "sunset"],
+	"hourly": ["temperature_2m", "apparent_temperature", "cloud_cover", "temperature_80m", "snow_depth", "visibility", "rain", "weather_code"],
+	"past_days": 5,
 }
-'''url="https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
-#openwheater#-https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longtude}&exclude={exclude_port}&appid={API_key}
+
+'''url="https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly
+=temperature_2m"#openwheater#-https://api.openweathermap.org/data/3.0/onecall?
+lat={latitude}&lon={longtude}&exclude={exclude_port}&appid={API_key}
 '''
 response=requests.get(url,params=params)
 
 if response.status_code==200:
     data=response.json()
-    print(data)
+    #print(data)
 else:
     print(f"Error: {response.status_code},{response.text}")
+
+input("press enter to continue . . .")
+
+print(f"Coordinates:{data['latitude']}°N {data['longitude']}°E")
+print(f"Elevation:{data['elevation']} m asl")
+print(f"Timezone difference to GMT+0:{data['utc_offset_seconds']}s")
+
+
+'''
+hourly=data['date]
+hourly_temperature_2m = np.array(hourly['temperature_2m'])
+hourly_apparent_temperature = np.array(hourly['apparent_temperature'])
+hourly_cloud_cover = np.array(hourly['cloud_cover'])
+hourly_temperature_80m = np.array(hourly['temperature_80m'])
+hourly_snow_depth = np.array(hourly['snow_depth'])
+hourly_visibility = np.array(hourly['visibility'])
+hourly_rain = np.array(hourly['rain'])
+hourly_weather_code = np.array(hourly['weather_code'])
+'''
+hourly ={
+"date":pd.to_datetime(data['hourly']['time']),    
+"hourly_temperature_2m": data['hourly']['temperature_2m'],
+"hourly_apparent_temperature" :  data['hourly']['apparent_temperature'],
+"hourly_cloud_cover"  : data['hourly']['cloud_cover'],
+"hourly_temperature_80m"  :  data['hourly']['temperature_80m'],
+"hourly_snow_depth"  :  data['hourly']['snow_depth'],
+"hourly_visibility"  :  data['hourly']['visibility'],
+"hourly_rain"  :  data['hourly']['rain'],
+"hourly_weather_code"  :  data['hourly']['weather_code']
+}
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows',50)
+pd.set_option('display.width',1000)
+
+df=pd.DataFrame(hourly)
+
+print(df)
