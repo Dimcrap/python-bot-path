b66a6dfbb4ab1778342f50d9a534fc2428221601
dimcrap <mamad.rahmani235@gmail.com>
2026-01-05 15:37:02 +0330

    implementing main structure of project - testing the user input pipline

--- chatsimulator/main.py
+++ chatsimulator/main.py
@@ -2,22 +2,45 @@
 messges=["welcome to this  environments","how can i help you"]
 
 class Chatbot:
-   def __int__(self):
-    self.state="start"
    
-   help="this is a testing chatbot \nthere will be options available:\n/help\t/cancel\n/start"
+   def __init__(self):
+    self.state="start"
+
+    
+
+   options="/help\t/cancel\t/start\n/calculate" 
+   help="this is a testing chatbot \nthere will be options available:\n"+options
 
    @staticmethod
-   def mainfunction():
+   def calc():
         numb=input("enter a number")
-        print(numb%2)
+        print(numb*0.50)
+        print("\n",help)
       
    def handle_message(self,msg):
     if self.state=="start":
         if msg=="/start":
-            Chatbot.mainfunction()
+            #Chatbot.calc()
+            print("welome !you are already in process \noption:",self.options)
         if msg=="/help":
             return help
         if msg=="/cancel":
             self.state="stop"
-        
+        if msg=="calculate":
+           Chatbot.calc()
+        else:
+           print("unavalid input") 
+    else:
+       print("the bot is stoppeted!")
+
+
+input("press enter to start ...")
+bot=Chatbot()
+while True:
+   usrInput=input(bot.help+'\n')
+   if usrInput=="/cancel":
+      bot.handle_message(usrInput)
+      break
+   else:
+      bot.handle_message(usrInput)
+       
