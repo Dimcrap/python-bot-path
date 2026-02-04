0d97e4e95d6f42bfaf92af9cf37cb66f320dbb8e
dimcrap <mamad.rahmani235@gmail.com>
2026-02-04 17:21:53 +0330

    completing the features and testing the module compeletly

--- cmdparser/main.py
+++ cmdparser/main.py
@@ -1,12 +1,11 @@
+import datetime
+import ast
 
 deposits=[]
 raks=[{"number":"001","status":"filled"},{"number":"002","status":"not-filled"},
-       {"number":"003","status":"filled"},{"number":"004","status":"not-filled"}]
+       {"number":"003","status":"filled"},{"number":"004","status":"not-filled"},
+       {"number":"005","status":"not-filled"},{"number":"006","status":"not-filled"}]
 
-#testcases: "/start","/stop","/time",
-# "/status_rak -number-",
-# "/deposit -name- -amount-",
-# /find_empty_rak -[number]=default=>1-  
 
 class commandParser:
 
@@ -16,22 +15,87 @@ class commandParser:
 
     def parser(self,input):
         command={"command":"","countflag":"","values":{}}
-        if input.find("find_empty_rak"):
-            if input[17]=="-":
-                details=(input[18:input.len()]).split()
+        fullStr=input.split()
+
+        if input.find("find_empty_rak")!=-1:
+            if len(input)>15:
+                details=(input[17:len(input)]).split()
                 command["command"]="find_rak"
                 command["countflag"]=details[0]                
-                self.router(command)
             else:
-                self.router("find")
-        if input.find("deposit"):
-            fullStr=raks.split()
-            self.router("deposit",{"name":fullStr[1],"amount":fullStr[2]})
-        if input.find()    
-            
+                command["command"]="find_rak"
+                command["countflag"]="1"
+        elif input.find("deposit")!=-1:
+            command["command"]="deposit"
+            command["values"]={"name":fullStr[1],"amount":fullStr[2]}
+        elif input.find("status_rak")!=-1:
+                command["command"]="status"
+                command["values"]={"number":fullStr[1]}
+        elif not input.find("start") or not input.find("stop") or not input.find("time"):
+            command["command"]="error"
+        else:
+            command["command"]=fullStr[0]
+        self.router(command)
 
+    def router(self,commandDict):
+        if(commandDict["command"])=="find_rak":
+            self.rakfinder(commandDict["countflag"])
+        elif commandDict["command"]=="deposit":
+            self.depositor(commandDict["values"]) 
+        elif commandDict["command"]=="status":
+            self.rakreport(commandDict["values"]["number"])
+        elif commandDict["command"]=="error":
+            print("unvalid input detected!\nprocess failed")
+        else:
+            self.headfunc(commandDict["command"]) 
+                               
+    def rakfinder(self,instruct):    
+        raks_namelist=[]
+        start=0
+        for x in range(int(instruct)):
+                for index in range(start,len(self.raks)):
+                    if (self.raks[index]["status"]=="not-filled"):
+                        raks_namelist.append(self.raks[index]["number"])
+                        start=index+1
+                        break
 
-    def router(slef,command,dict):
+        print(raks_namelist)         
 
-    def is_validate();    
+    def depositor(self,instruct):        
+        self.deposits.append(instruct)
+         
+    def rakreport(self,raknumber):
+         status=""
+         for rak in self.raks:
+                if rak["number"]==raknumber:
+                    status=rak["status"]
+         print(f"rak number {raknumber} is {status}")
+
+    def headfunc(self,cmd):
+         if cmd=="/start":
+              print("welcome to the process -guides . . .")
+         elif cmd=="/stop":
+              print("process stopped=>reuse it with starting it")
+         elif cmd=="/time":
+              print(datetime.datetime.now())
+         else:
+              print("unvalid command recieved\nprocess failed")     
+
+#testcases: "/start","/stop","/time",
+# "/status_rak -number-",
+# "/deposit -name- -amount-",
+# /find_empty_rak -[number]=default=>1-  
+
+cmdparser=commandParser(deposits,raks)
+
+#cmdparser.parser("/start")
+#cmdparser.parser("/stop")
+#cmdparser.parser("/time")
+
+#cmdparser.parser("/status_rak 004")
+
+#cmdparser.parser("deposit ahlam 2355")
+#print(deposits)
+
+cmdparser.parser("/find_empty_rak -4")
 
